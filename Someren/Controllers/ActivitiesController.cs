b1ca9ddf0474670b65cae55a87f5e141c0de51af
cristhelsolomon <cristhelsolomon2004@gmail.com>
2026-03-20 02:30:37 +0100

    A running activity management feat: matches the student management ui fix: error handling updates, remove unecedssary comments and shorten methods.

---
+++ Someren/Controllers/ActivitiesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Someren.Models;
+using Someren.Repositories;
+
+namespace Someren.Controllers;
+
+public class ActivitiesController : Controller
+{
+    private readonly IActivityRepository _activityRepo;
+
+    public ActivitiesController(IActivityRepository activityRepo) => _activityRepo = activityRepo;
+
+    public IActionResult Index(string? searchTerm) => View(_activityRepo.GetAll(searchTerm));
+
+    // GET: Show Create Form
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public IActionResult Create(Activity activity)
+    {
+        if (!ModelState.IsValid) return View(activity);
+        _activityRepo.Add(activity);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Show Edit Form
+    public IActionResult Edit(int id)
+    {
+        var activity = _activityRepo.GetById(id);
+        return activity == null ? NotFound() : View(activity);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Activity activity)
+    {
+        if (!ModelState.IsValid) return View(activity);
+        _activityRepo.Update(activity);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Show Delete Confirmation Page
+    public IActionResult Delete(int id)
+    {
+        var activity = _activityRepo.GetById(id);
+        return activity == null ? NotFound() : View(activity);
+    }
+
+    // POST: Actually perform the delete
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        _activityRepo.Delete(id);
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Someren/Repositories/IActivityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Someren.Models;
+
+namespace Someren.Repositories
+{
+    public interface IActivityRepository
+    {
+        List<Activity> GetAll(string? searchTerm); //with a seachterm
+
+        void Add (Activity activity);
+
+        Activity? GetById(int id); //find activity using id 
+
+        void Update (Activity activity);
+
+        void Delete (int id);
+
+        bool ActivityNameExists(string name, int? excludedId = null); /*stops the database from doing bool yes 
+                                                                        when editing an existing activity*/
+    }
+}
